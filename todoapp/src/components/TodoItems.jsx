3e15d183b53190fa224a636073effa2af5520ee7
Keshav7Chauhan <141233286+Keshav7Chauhan@users.noreply.github.com>
2024-07-09 23:38:21 +0530

    change code

---
+++ todoapp/src/components/TodoItems.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import tick from '../assets/tick.png'
+import not_tick from '../assets/not_tick.png'
+import delete_icon from '../assets/delete.png'
+
+
+
+const TodoItems = () => {
+  return (
+    <div className='flex ite my-3 gap-2'>
+      <div className='flex flex-1 items-center cursor-pointer'>
+        <img src={tick} alt="" className='w-7' />
+        <p className='text-slate-700 ml-4 text-[17px]'>Learn Coding</p>
+      </div>
+    </div>
+  )
+}
+
+export default TodoItems
